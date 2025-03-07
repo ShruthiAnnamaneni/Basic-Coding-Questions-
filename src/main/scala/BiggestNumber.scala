58c1c965a3fc45424f48fbafb0e961fe5bb22c0c
Shruthi <ashruthi09@gmail.com>
2025-03-07 16:45:06 +0530

    Biggest Number

---
+++ src/main/scala/BiggestNumber.scala
@@ -0,0 +1,19 @@
+object BiggestNumber {
+
+  def main(args:Array[String]):Unit= {
+// 3. Declare and initialize 3 three variable and print the biggest number.
+    val a = 10
+    val b = 15
+    val c = 30
+    val greater =
+      if (a > b & a > c) {
+        print("the largest number is "+a)
+      }
+      else if (b > a & b >c) {
+        print("the largest number is "+b)
+      }
+      else{
+        print ("the largest number is "+ c)
+      }
+  }
+}
