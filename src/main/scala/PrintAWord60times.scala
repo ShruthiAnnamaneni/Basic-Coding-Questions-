f3c2e7b87794627d495c5423cefa1cb07b0225a4
Shruthi <ashruthi09@gmail.com>
2025-03-07 16:56:57 +0530

    Print A word 60 times

---
+++ src/main/scala/PrintAWord60times.scala
@@ -0,0 +1,8 @@
+object PrintAWord60times {
+ // 7.Write a program to print “SEEKHO BIGDATA”for 60 times.
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 60 ) {
+      println("seekho bigdata")
+    }
+  }
+}
