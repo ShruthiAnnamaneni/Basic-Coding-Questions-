d3fbdfd8c9ac5a7f445230bed4e5202945971f1c
Shruthi <ashruthi09@gmail.com>
2025-03-08 06:27:00 +0530

    Problem 24

---
+++ src/main/scala/Problem24.scala
@@ -0,0 +1,9 @@
+object Problem24 {
+  //24. Write programs to print the following series. 5,10,5,10,5,10,5 for 7 times
+    def main(args:Array[String]):Unit={
+      for(i<-1 to 7){
+        print("5,10,")
+      }
+    }
+
+}
