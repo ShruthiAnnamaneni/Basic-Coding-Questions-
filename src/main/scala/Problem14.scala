58e6c35c889b16f937a673ddd278726e31d234ac
Shruthi <ashruthi09@gmail.com>
2025-03-08 06:23:09 +0530

    Problem 14

---
+++ src/main/scala/Problem14.scala
@@ -0,0 +1,8 @@
+object Problem14 {
+//14. Write a program to print alternate even numbers from 20 to 140. Like (22,26,30...)
+  def main(args:Array[String]):Unit={
+    for(i<-22 to 140 by 4){
+      print(i+" ")
+    }
+  }
+}
