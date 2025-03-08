c1b9fb31f15e3d248b9b25bd8026900101509088
Shruthi <ashruthi09@gmail.com>
2025-03-08 06:24:32 +0530

    Problem 19

---
+++ src/main/scala/Problem19.scala
@@ -0,0 +1,12 @@
+object Problem19 {
+//19. Write programs to sum of the following Series. 52 + 62 + 72 +..........+1022.
+  def main(args:Array[String]):Unit={
+    var i=1
+    var num=42
+    while(i<99){
+      num=num+10
+      print(num+" + ")
+      i=i+1
+    }
+  }
+}
